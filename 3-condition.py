1be5d8dbbaf38831afc85ffbaacafca56c629220
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-05-05 11:16:52 +0600

    Add files via upload

---
+++ 3-condition.py
@@ -0,0 +1,18 @@
+
+# Declaring, assigning and incrementing variable
+n = 5
+n = n + 1
+n += 1
+print(n)
+
+a = 5
+b = 2
+
+# Condition
+
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is equal to b")
