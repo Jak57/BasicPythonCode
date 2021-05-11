be7e2d4c376467312b4809a7a0b9e4d019861aeb
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-05-11 14:49:51 +0600

    Add files via upload

---
+++ 20-number.py
@@ -0,0 +1,15 @@
+
+numbers =[4, 8, 0, 9, 2]
+
+if 6 in numbers:
+    print("Yes")
+else:
+    print("No")
+
+name = ["Jakir", "Ron"]
+
+s = "Jak"
+if s in name:
+    print("Found")
+else:
+    print("Not found")
