e2f4b83d4d16af614db4379a4528a44e8bc03d15
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-05-11 14:52:17 +0600

    Add files via upload

---
+++ 25-speech.py
@@ -0,0 +1,15 @@
+#import cs50
+#import pyttsx3
+
+#engine = pyttsx3.init()
+#engine.say("hello, world")
+#engine.runAndWait()
+
+#name = input("What's your name? ")
+#engine.say(f"hello, {name}")
+
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("hello, world")
+engine.runAndWait()
