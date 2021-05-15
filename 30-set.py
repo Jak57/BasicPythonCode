f63f40c3d7c8e16a885957c8a9c6ac65fc95ca01
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-05-15 16:02:07 +0600

    Add files via upload

---
+++ 30-set.py
@@ -0,0 +1,21 @@
+# set of unique elements
+s = set()
+
+# adding element
+s.add(3)
+s.add(0)
+s.add(9)
+s.add(-9)
+s.add(100)
+s.add(100)
+
+print(s)
+
+# removing element
+s.remove(-9)
+print(s)
+
+# print length
+print(len(s))
+
+
